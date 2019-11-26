198dd818282f558954477d610d00a4acf1db75d5
tm1611 <tmeiendresch@gmx.de>
2019-11-26 16:30:29 +0100

    initial commit
    upload data to use in sagemaker (especially for deepstate)

---
+++ gluonts11-m4_subsets_sagemaker/utils_gluonts10_deepar.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+
+import mxnet as mx
+
+# utils
+from utils_gluonts10 import get_dataset
+
+# gluonts
+from gluonts.trainer import Trainer
+from gluonts.evaluation.backtest import make_evaluation_predictions
+from gluonts.evaluation import Evaluator
+
+# DeepAREstimator
+from gluonts.model.deepar import DeepAREstimator
+
+######################
+### deepar wrapper ###
+def deepar(data="m4_quarterly", seed=42, epochs=100, batches=50):
+
+    dataset = get_dataset(data, regenerate=False)
+    mx.random.seed(seed)
+    np.random.seed(seed)
+
+    trainer = Trainer(
+        ctx=mx.cpu(0),
+        epochs=epochs,
+        num_batches_per_epoch=batches,
+        learning_rate=1e-3,
+    )
+
+    cardinality = int(dataset.metadata.feat_static_cat[0].cardinality)
+    estimator = DeepAREstimator(
+        trainer=trainer,
+        cardinality=[cardinality],
+        context_length=dataset.metadata.prediction_length,
+        prediction_length=dataset.metadata.prediction_length,
+        freq=dataset.metadata.freq,
+        use_feat_static_cat=True
+    )
+
+    # predictor = estimator.train(training_data=dataset.train,
+    #                             validation_data=dataset.test)
+    predictor = estimator.train(training_data=dataset.train)
+
+    forecast_it, ts_it = make_evaluation_predictions(
+        dataset.test, predictor=predictor, num_samples=100
+    )
+
+    agg_metrics, item_metrics = Evaluator()(
+        ts_it, forecast_it, num_series=len(dataset.test)
+    )
+
+    metrics = ["MASE", "sMAPE", "MSIS", "wQuantileLoss[0.5]", "wQuantileLoss[0.9]"]
+    output = {key: round(value, 8) for key, value in agg_metrics.items() if key in metrics}
+
+    output["seed"] = seed
+    output["epochs"] = epochs
+    output["num_batches"] = batches
+
+    df = pd.DataFrame([output])
+
+    return df
